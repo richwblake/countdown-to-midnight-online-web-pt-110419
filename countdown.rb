9b2451f0640139a3e484ecfeeef17e740460d79c
Wills Blake <richwblake@gmail.com>
2019-11-04 17:59:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,8 @@ def countdown( number_of_seconds )
 end
 
 def countdown_with_sleep( number_of_seconds )
-  
+  until number_of_seconds == 0 do
+    puts "#{number_of_seconds} SECONDS!"
+    sleep(1)
+  end
 end
