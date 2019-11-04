d68892343ceea0a6aa9c41b49e0f81e813c10ab0
Wills Blake <richwblake@gmail.com>
2019-11-04 18:00:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,7 @@ def countdown_with_sleep( number_of_seconds )
   until number_of_seconds == 0 do
     puts "#{number_of_seconds} SECONDS!"
     sleep(1)
+    number_of_seconds -= 1
   end
   "HAPPY NEW YEAR!"
 end
